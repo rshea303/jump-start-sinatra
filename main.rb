a12211985cca56c6e8799fe428b67f6757adbe20
Rich Shea <richcshea@gmail.com>
2014-12-11 22:14:35 -0700

    added database configuration

--- main.rb
+++ main.rb
@@ -4,6 +4,15 @@ require 'slim'
 require 'sass'
 require './song'
 
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 configure do
   enable :sessions
   set :username, 'frank'
